b7fb4b0db0eec888b085e59182b6c21b36921d4f
suhaniii411 <suhaniagarwal411@gmail.com>
2026-03-27 18:21:58 +0530

    Add CI/CD pipeline

--- src/db.js
+++ src/db.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const initDB = async () => {
